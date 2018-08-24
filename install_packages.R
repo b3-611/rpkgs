0469731ef6abbb125c8fa64bf1b6a93577868c36
Kenji Sato <kenji@kenjisato.jp>
2018-08-25 08:14:12 +0900

    Fix bugs.

--- install_packages.R
+++ install_packages.R
@@ -1,7 +1,7 @@
 # Script to install necessary packages.
 
-csv <- "extdata/pkgs.txt"
+pkg_list <- "https://raw.githubusercontent.com/b3-611/rpkgs/master/data/pkgs.txt"
 repo = "https://cloud.r-project.org"
 
 pkgs <- read.csv(pkg_list, header = FALSE, stringsAsFactors = FALSE)
-install.packages(pkgs, dependencies = TRUE, repos = repo)
+install.packages(pkgs[, 1], dependencies = TRUE, repos = repo)
